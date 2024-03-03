3b0e7053dd96d4da8e17ab76695ff812333ca68f
andrea lozano <lozano.andrea.1977@gmail.com>
2024-03-03 14:03:32 -0500

    add: REST product

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '../../../database';
+import { SHOP_CONSTANTS, db } from '../../../database';
 import { Product } from '../../../models';
 import { IProduct } from '../../../interfaces';
 
-type Data = {name: string} | IProduct[]
+type Data = { name: string } | IProduct[];
 
 export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
@@ -13,18 +13,21 @@ export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
       return res.status(400).json({
         name: 'Bad request',
       });
-  
   }
 }
 
-
 async function getProducts(req: NextApiRequest, res: NextApiResponse<Data>) {
-  await db.connect()
-  const products = await Product.find()
+  const { gender = 'all' } = req.query;
+  let condition = {};
+  if (gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+    condition = { gender };
+  }
+  await db.connect();
+  const products = await Product.find(condition)
     .select('title images price inStock slug -_id')
     .lean();
 
   await db.disconnect();
 
-  return res.status(200).json(products)
+  return res.status(200).json(products);
 }
